cd80c3b1940f70f60b0c529c81cb1a07174ff470
tbence94 <tihanyibence@gmail.com>
2018-03-16 19:32:58 +0100

    config

---
+++ src/main/java/hu/pemik/dcs/restserver/Config.java
@@ -0,0 +1,6 @@
+package hu.pemik.dcs.restserver;
+
+public class Config {
+    public final static String DATABASE_FILE = "/rendsz/RESTServer/db.ser";
+    public final static String REST_SERVER_URL = "http://localhost:8080/";
+}
