fd3008c8e85678ba519ab9a2defef7ab454eb7fc
tbence94 <tihanyibence@gmail.com>
2018-03-17 16:00:57 +0100

    add reseed option

--- src/main/java/hu/pemik/dcs/restserver/ApplicationMain.java
+++ src/main/java/hu/pemik/dcs/restserver/ApplicationMain.java
@@ -1,11 +1,18 @@
 package hu.pemik.dcs.restserver;
 
 import hu.pemik.dcs.restserver.database.Database;
+import hu.pemik.dcs.restserver.database.Seeder;
 
 public class ApplicationMain {
 
     public static void main(String[] args) {
+
         Database db = Database.connect();
+
+        if (args[0].equals("--reseed")) {
+            Seeder.reseed();
+        }
+
         db.dump();
 
         ServiceServer server = new ServiceServer();
