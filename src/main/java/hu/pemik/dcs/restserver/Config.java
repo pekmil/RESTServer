561c3dbd14a79efa4b5d116abf4f7decc26dbec2
tbence94 <tihanyibence@gmail.com>
2018-03-17 13:33:40 +0100

    remove ignored config

--- src/main/java/hu/pemik/dcs/restserver/Config.java
+++
@@ -1,6 +0,0 @@
-package hu.pemik.dcs.restserver;
-
-public class Config {
-    public final static String DATABASE_FILE = "/rendsz/RESTServer/db.ser";
-    public final static String REST_SERVER_URL = "http://localhost:8080/";
-}
